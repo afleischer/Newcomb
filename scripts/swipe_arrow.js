a88c3893588f883e61c91467fb4430fd09f93750
afleischer <afleischer1337@gmail.com>
2018-11-27 17:09:38 -0800

    fixed horizontal scroll

---
+++ scripts/swipe_arrow.js
@@ -0,0 +1,17 @@
+function swipe(){
+		console.log("clicked!");
+}
+/*
+window.onload = function(){
+	var el = document.querySelector('.swipe_indicator');
+		el.addEventListener("click", function(){
+		console.log("clicked!");
+	})
+}
+
+function swipe(){
+	var el = document.querySelector('.swipe_indicator');
+		el.addEventListener("click", function(){
+		console.log("clicked!");
+	})
+*/
